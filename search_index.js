0e08077f81375ca596ebe2fa41d290f1c3ec7d85
David Varela <00.varela.david@gmail.com>
2019-02-11 21:49:39 -0800

    still testing

--- search_index.js
+++ search_index.js
@@ -17,19 +17,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "AboutMe/#THIS-IS-A-TEST-1",
-    "page": "About Me",
-    "title": "THIS IS A TEST",
-    "category": "section",
-    "text": ""
-},
-
-{
     "location": "AboutMe/#Contact-1",
     "page": "About Me",
     "title": "Contact",
     "category": "section",
-    "text": "github"
+    "text": "github\ntwitter"
 },
 
 {
@@ -41,99 +33,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "dlog/#",
-    "page": "Foo",
-    "title": "Foo",
+    "location": "first_post/#",
+    "page": "First Post",
+    "title": "First Post",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "dlog/#dlog:-the-engine-behind-this-blog-1",
-    "page": "Foo",
-    "title": "dlog: the engine behind this blog",
+    "location": "first_post/#This-is-a-Test-Post-1",
+    "page": "First Post",
+    "title": "This is a Test Post",
     "category": "section",
-    "text": "dlog is a simple blog engine that integrates well with other linux tools."
-},
-
-{
-    "location": "dlog/#goals-1",
-    "page": "Foo",
-    "title": "goals",
-    "category": "section",
-    "text": "creating content should be no more complex than editing a markdown file\nintegrates well with tools following the UNIX philosophy\nlight payload\nminimal, but complete API\na focus on the readablility of the end product"
-},
-
-{
-    "location": "dlog/#api-1",
-    "page": "Foo",
-    "title": "api",
-    "category": "section",
-    "text": "new/remove: register or remove a post from dlog\'s database\nlive_demo: serve the a debuggable version of the site locally\nlive_prod: serve the end product locally\npublish/unpublish: mark/unmark posts to be included in the production payload\nedit: edit the markdown version of a post (with a text editor of the user\'s choice)\nprint: print out a line-oriented representation of dlog\'s database"
-},
-
-{
-    "location": "dlog/#other-features-1",
-    "page": "Foo",
-    "title": "other features",
-    "category": "section",
-    "text": "view logging output of internal commands (eg with less +F)\nlive, local view of site\nskeleton.css is used as a base, easy to configure from that point"
-},
-
-{
-    "location": "dlog/#example-workflow-1",
-    "page": "Foo",
-    "title": "example workflow",
-    "category": "section",
-    "text": "Edit markdown files while checking local mirror as a reference. When things look good, push to github. That\'s it!"
-},
-
-{
-    "location": "dlog/#acknowledgements-1",
-    "page": "Foo",
-    "title": "acknowledgements",
-    "category": "section",
-    "text": "skeleteon.css\nmithril\nmistletoe\nnix\nentr"
-},
-
-{
-    "location": "SystemdD-nspawn/#",
-    "page": "Bar",
-    "title": "Bar",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "SystemdD-nspawn/#Setting-up-ssh-inside-of-systemd-nspawn-1",
-    "page": "Bar",
-    "title": "Setting up ssh inside of systemd-nspawn",
-    "category": "section",
-    "text": ""
-},
-
-{
-    "location": "SystemdD-nspawn/#Motivation-1",
-    "page": "Bar",
-    "title": "Motivation",
-    "category": "section",
-    "text": "The adoption of systemd by the major linux distributions   means that systemd-nspawn is the most painless way to run a container. I do most of my work inside of containers;   which means multiple interactive processes per container. It is cumbersome to interact with a container through a single, dumb IO stream. In general, there are two solutions to this problem:run a multiplexer (e.g. tmux) after an ssh connection is made\nconnect to the container using multiple ssh connections; one connection per processBoth options have strengths and weaknesses. Two weaknesses of terminal multiplexers:   they consume key-bindings   and they add complexity to the IO stream. I recommend setting up ssh for the added flexibility   even if you choose to run a multiplexer."
-},
-
-{
-    "location": "SystemdD-nspawn/#Instructions-1",
-    "page": "Bar",
-    "title": "Instructions",
-    "category": "section",
-    "text": "Outside of the containermake sure the systemd-nspawn container is running: systemd-nspawn <...>Inside the containerinstall an ssh daemon: pacman -S openssh\nof course if you are not running arch, replace pacman with apt, yum, ...\nconfigure ssh:  a good starting point is this guide\ngenerate ssh keys locally\ntransfer keys to the container\nconfigure sshd\ndisable root login\ndisable password login\nenable the ssh daemon through systemd: systemctl enable sshd\ndo not forget to start the daemon!: systemctl start sshdOutside the containerlogin via ssh: ssh <username>@localhost"
-},
-
-{
-    "location": "SystemdD-nspawn/#TODO-1",
-    "page": "Bar",
-    "title": "TODO",
-    "category": "section",
-    "text": "enable systemd-nspawn automatically"
+    "text": "one\ntwo\nthree"
 },
 
 ]}

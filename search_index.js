e9e42d500a70fbee144d7afe0d079443d63d4d5b
David Varela <00.varela.david@gmail.com>
2019-02-12 13:50:04 -0800

    Draft for first post

--- search_index.js
+++ search_index.js
@@ -33,59 +33,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "second_post/#",
-    "page": "Second Post",
-    "title": "Second Post",
+    "location": "declarative_repl/#",
+    "page": "Declarative or Imperative",
+    "title": "Declarative or Imperative",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "second_post/#This-is-also-a-test-1",
-    "page": "Second Post",
-    "title": "This is also a test",
+    "location": "declarative_repl/#Declarative-or-Imperative-[12-Feb-2019]-1",
+    "page": "Declarative or Imperative",
+    "title": "Declarative or Imperative [12 Feb 2019]",
     "category": "section",
-    "text": "one\ntwo\nthreexs = [1,2,3]\nfunction foo(x::Int; y::Bool=false)\n    x = x + 1\n    return x\nend"
-},
-
-{
-    "location": "second_post/#Some-more-testing-1",
-    "page": "Second Post",
-    "title": "Some more testing",
-    "category": "section",
-    "text": "one\ntwo\nnested\nthree\nnest one\nnest twobla bla bla."
-},
-
-{
-    "location": "second_post/#subsection-1",
-    "page": "Second Post",
-    "title": "subsection",
-    "category": "section",
-    "text": "Research into packet switching, one of the fundamental Internet technologies started in the early 1960s in the work of Paul Baran,[13] and packet embeded link switched networks such as the NPL network by Donald Davies,[14] ARPANET, Tymnet, the Merit Network,[15] Telenet, and CYCLADES,[16][17] were developed in the late 1960s and 1970s using a variety of protocols.[18] The ARPANET project led to the development of protocols for internetworking, by f(x) = x + 1 which multiple separate networks could be joined into a network of networks.[19] ARPANET development began with two network nodes which were interconnected between the Network Measurement Center at the University of California, Los Angeles (UCLA) Henry Samueli School of Engineering and Applied Science directed by Leonard Kleinrock, and the NLS system at SRI International (SRI) by Douglas Engelbart in Menlo Park, California, on 29 October 1969.[20] The third site was the Culler-Fried Interactive Mathematics Center at the University of California, Santa Barbara, followed by the University of Utah Graphics Department. In an early sign of future growth, fifteen sites were connected to the young ARPANET by the end of 1971.[21][22] These early years were documented in the 1972 film Computer Networks: The Heralds of Resource Sharing.bla bla bla bla. bla bla bla bla. bla bla bla bla. bla bla bla bla."
-},
-
-{
-    "location": "second_post/#And-what-about-more-testing?-1",
-    "page": "Second Post",
-    "title": "And what about more testing?",
-    "category": "section",
-    "text": "this should be a quote"
-},
-
-{
-    "location": "first_post/#",
-    "page": "First Post",
-    "title": "First Post",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "first_post/#This-is-a-Test-Post-1",
-    "page": "First Post",
-    "title": "This is a Test Post",
-    "category": "section",
-    "text": "one\ntwo\nthree"
+    "text": "Now that the REPL for Pkg has stabilized,   I want to take the opportunity to analyze some design decisions. A core part of the REPL is the specification for the behavior of each command.An example of the declarative specification for the rm operation in Pkg:[ :kind        => CMD_RM,\n  :name        => \"remove\",\n  :short_name  => \"rm\",\n  :handler     => do_rm!,\n  :arg_count   => 1 => Inf,\n  :arg_parser  => parse_pkg,\n  :option_spec => OptionDeclaration[\n      [:name => \"project\",  :short_name => \"p\", :api => :mode => PKGMODE_PROJECT],\n      [:name => \"manifest\", :short_name => \"m\", :api => :mode => PKGMODE_MANIFEST],\n  ],\n  :completions => complete_installed_packages,\n  :description => \"remove packages from project or manifest\","
 },
 
 ]}
